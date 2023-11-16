7bd6dda8b67aeb846600589c8e7e5286eb72fd35
Vladimir Satdarov <vladimir.satdarov@gmail.com>
2023-11-16 14:21:00 +0100

    added getMessage() method

--- src/main/java/de/doccheck/Main.java
+++ src/main/java/de/doccheck/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa.guru!");
     }
+
+    private static String getMessage() {
+        return "Hello qa.guru!";
+    }
 }
